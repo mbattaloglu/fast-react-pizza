c3e0ca25b71c19d378d2780d909f454de89259e2
mbattaloglu <mmbattaloglu@gmail.com>
2024-03-14 02:47:10 +0300

    files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fast React Pizza
+From the Jonas Schmedtmann's course ["The Ultimate React Course 2024: React, Redux & More"](https://www.udemy.com/course/the-ultimate-react-course/).
---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,25 @@
+import { getMenu } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <>
+      <h2 className="text-center text-2xl my-5">Our Pizzas</h2>
+      <ul className="divide-y divide-stone-200 px-2">
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
